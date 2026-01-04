e290af4448bd99a49c4729425f91a0f839cd1859
PouKehYongMRT234002 <poukehyong@graduate.utm.my>
2026-01-04 20:42:23 +0800

    Dataset used to train v1 and v2 prediction models.

---
+++ train_model_v1.py
@@ -0,0 +1,17 @@
+#•	v1 — Baseline Model
+# Uses only: area → price
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+df = pd.read_csv("Housing.csv")
+
+X = df[["area"]]
+y = df["price"]
+
+model_v1 = LinearRegression()
+model_v1.fit(X, y)
+
+joblib.dump(model_v1, "model_v1.pkl")
+print("Baseline model saved.")
